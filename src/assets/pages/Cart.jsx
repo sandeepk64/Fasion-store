8c5f38a98e8e29f7527bffb2fe05549fb4d4fc32
sandeepk64 <vibranium62@gmail.com>
2024-05-25 10:56:17 +0530

    complete

--- src/assets/pages/Cart.jsx
+++ src/assets/pages/Cart.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { decQuantity, emptyCart, incQuantity, removeCartItem } from '../redux/slices/cartSlice'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Cart = () => {
   const ourCart = useSelector(state=>state.cartReducer)
+  const [cartTotal,setCartTotal]=useState(0)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+   if(ourCart?.length>0){
+    setCartTotal(ourCart?.map(item=>item.totalPrice).reduce((t1,t2)=>t1+t2))
+   }else{
+    setCartTotal(0)
+   }
+  },[ourCart])
+
+  const handleDecrement =(product)=>{
+    if(product.quantity>1){
+      dispatch(decQuantity(product.id))
+    }else{
+      dispatch(removeCartItem(product.id))
+    }
+  }
+
+  const checkout =()=>{
+    dispatch(emptyCart())
+    alert("Order placed successfully. Thank you for purchasing with us!!!")
+    navigate('/')
+  }
   return (
     <>
       <Header/>
@@ -34,28 +61,40 @@ const Cart = () => {
                     <td><img width={'50px'} src={product?.thumbnail} alt="" /></td>
                     <td>
                       <div className="d-flex">
-                        <button className='btn fw-bolder'>-</button>
+                        <button onClick={()=>handleDecrement(product)} className='btn fw-bolder'>-</button>
                         <input value={product?.quantity} style={{width:'50px'}} className='fw-bolder me-1 ms-1' type="text" readOnly />
-                        <button className='btn fw-bolder'>+</button>
+                        <button onClick={()=>dispatch(incQuantity(product?.id))} className='btn fw-bolder'>+</button>
                       </div>
                     </td>
                     <td>$ {product?.totalPrice}</td>
                     <td>
-                      <button className='btn'><i className='fa-solid fa-trash text-danger'></i></button>
+                      <button onClick={()=>dispatch(removeCartItem(product?.id))} className='btn'><i className='fa-solid fa-trash text-danger'></i></button>
                     </td>
                   </tr>
                     ))
                   }
                 </tbody>
               </table>
+              <div className="float-end">
+                <button onClick={()=>dispatch(emptyCart())} className='btn btn-danger me-2'>EMPTY CART</button>
+                <Link to={'/'} className='btn btn-primary'>SHOP MORE</Link>
+              </div>
             </div>
-            <div className="col-lg-4"></div>
+            <div className="col-lg-4">
+              <div className="border rounded p-3">
+                <h4>Total Amount : <span className='text-danger'>$ {cartTotal}</span></h4>
+                <hr />
+                <div className="d-grid">
+                  <button onClick={checkout} className='btn btn-success'>checkout</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         :
         <div style={{height:'60vh'}} className="d-flex justify-content-center align-items-center flex-column">
         <img height={'400px'} src="https://assets.materialup.com/uploads/38936b35-9823-47e1-98cc-fb8b5c1d962e/preview.jpg" alt="" />
-        <h3>Your Wishlist is Empty!!!</h3>
+        <h3>Your Cart is Empty!!!</h3>
       </div>
         }
       </div>
--- src/assets/redux/slices/cartSlice.js
+++ src/assets/redux/slices/cartSlice.js
@@ -14,9 +14,29 @@ const cartSlice = createSlice({
             }else{
                 state.push({...action.payload,quantity:1,totalPrice:action.payload.price})
             }
+        },
+        removeCartItem:(state,action)=>{
+            return state.filter(item=>item.id!=action.payload)
+        },
+        incQuantity:(state,action)=>{
+            const existingProduct = state.find(item=>item.id==action.payload)
+            existingProduct.quantity++
+            existingProduct.totalPrice = existingProduct.price * existingProduct.quantity
+            const remainingProducts = state.filter(item=>item.id!=existingProduct.id)
+            state = [...remainingProducts,existingProduct]
+        },
+        decQuantity:(state,action)=>{
+            const existingProduct = state.find(item=>item.id==action.payload)
+            existingProduct.quantity--
+            existingProduct.totalPrice = existingProduct.price * existingProduct.quantity
+            const remainingProducts = state.filter(item=>item.id!=existingProduct.id)
+            state = [...remainingProducts,existingProduct]
+        },
+        emptyCart: (state)=>{
+            return state = []
         }
     }
 })
 
-export const {addToCart} = cartSlice.actions
+export const {addToCart,removeCartItem,incQuantity,decQuantity,emptyCart} = cartSlice.actions
 export default cartSlice.reducer
